da99557ecb2769cc72a2742137d6a8391adaacbf
Ravi Karmakar <ravikarmkar94475@gmail.com>
2024-10-07 21:19:49 +0530

    about react-router-dom and reactContext

--- currencyConvertor/src/Hooks/useCurrencyInfo.js
+++ currencyConvertor/src/Hooks/useCurrencyInfo.js
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const convertCurrency = () => {
+const useCurrencyInfo = () => {
+  const [data, setData] = useState({});
+
   useEffect(() => {
     fetch(
       `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`
-    ).then()
-  }, []);
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+
+    // console.log(data);
+  }, [currency]);
+  console.log(data);
+  return data;
 };
+
+export default useCurrencyInfo;
---
+++ react-router/src/components/User/User.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function User() {
+  const { userid } = useParams();
+  return <div>user: {userid}</div>;
+}
+
+export default User;
